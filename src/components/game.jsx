7b99c8b702e2101b9ff8b76986848e7644b11a86
Pratik Sharma <pratiksharma@live.in>
2021-09-06 01:26:02 +0530

    game styling

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -11,17 +11,17 @@ const Game = ({ score, myChoice, setScore }) => {
     paper: ['rock'],
   };
 
+  const [counter, setCounter] = useState(3);
+  
   const newHousePick = () => {
     const choices = ['rock', 'paper', 'scissors'];
-    const houseChoice = choices[Math.floor(Math.random() * 3)];
-    setHouse(houseChoice);
+    setHouse(choices[Math.floor(Math.random() * 3)]);
   };
-
   useEffect(() => {
     newHousePick();
   }, []);
 
-  const result = () => {
+  const Result = () => {
     if (!myChoice || !house) return;
     if (myChoice === house) {
       return setPlayerWin('draw');
@@ -36,20 +36,53 @@ const Game = ({ score, myChoice, setScore }) => {
   };
 
   useEffect(() => {
-    result();
-  }, [house]);
+    const timer =
+      counter > 0
+        ? setInterval(() => {
+            setCounter(counter - 1);
+          }, 1000)
+        : Result();
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [counter, house]);
 
   return (
     <div className='game'>
-      My Choice : {myChoice}
-      <br />
-      House Choice:{house}
-      <br />
-      Result:{playerWin}
-      <br />
-      <Link to='/' onClick={() => setHouse()}>
-        Play Again
-      </Link>
+      <div className='game__you'>
+        <span className='text'>You Picked</span>
+        <div className={`icon icon--${myChoice} ${playerWin == 'win' ? `icon icon--${myChoice}--winner` : ''}`}></div>
+      </div>
+      {playerWin == 'win' && (
+        <div className='game__play'>
+          <span className='text'>You Win</span>
+          <Link to='/' className='play-again' onClick={() => setHouse()}>
+            Play Again
+          </Link>
+        </div>
+      )}
+      {playerWin == 'lose' && (
+        <div className='game__play'>
+          <span className='text'>You Lose</span>
+          <Link to='/' className='play-again' onClick={() => setHouse()}>
+            Play Again
+          </Link>
+        </div>
+      )}
+      {playerWin == 'draw' && (
+        <div className='game__play'>
+          <span className='text'>Draw</span>
+          <Link to='/' className='play-again' onClick={() => setHouse()}>
+            Play Again
+          </Link>
+        </div>
+      )}
+
+      <div className='game__house'>
+        <span className='text'>The House Picked</span>
+        {counter == 0 ? <div className={`icon icon--${house} ${playerWin == 'lose' ? `icon icon--${house}--winner` : ''}`}></div> : <div className='counter'>{counter}</div>}
+      </div>
     </div>
   );
 };
