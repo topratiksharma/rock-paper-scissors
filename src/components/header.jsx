536a48ffcd4d3da499797a6729bbd74c3b6360de
Pratik Sharma <pratiksharma@live.in>
2021-09-05 23:24:57 +0530

    header Styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 const Header = ({ score }) => {
   return (
     <div className='header'>
-      <h1>{score}</h1>
+      <div className='text'>
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+      </div>
+        <div className='score-box'>
+          <span>Score</span>
+          <div className='score-box__score'>{score}</div>
+        </div>
     </div>
   );
 };
