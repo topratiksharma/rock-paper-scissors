6579b67ab55ef2f7e3b459d4910ec8158273a78f
Pratik Sharma <pratiksharma@live.in>
2021-09-06 14:19:09 +0530

    footer added with read me

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='attribution'>Coded by Pratik Sharma</div>
+      <div className='attribution'>
+        Coded by <a href='https://github.com/topratiksharma/rock-paper-scissors' target="_blank">Pratik Sharma </a>{' '}
+      </div>
     </footer>
   );
 };
